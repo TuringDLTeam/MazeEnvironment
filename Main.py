b1ac217fdd203a32b88b5560e1c8ade783e8573c
Pooria Azami <pooriaazami@gmail.com>
2022-03-29 21:02:43 +0430

    test method added

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from EdgeGraph import EdgeGraph
+from MatrixImage import MatrixImage
 from utils import initialize_grid_graph, generate_maze
+
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -28,7 +31,25 @@ def test_maze_generator():
     print(generate_maze(10, 10))
 
 
+def test_matrix_image():
+    image = MatrixImage(10, 12)
+
+    plt.imshow(image.image)
+    plt.show()
+
+    plt.imshow(image.image)
+    plt.pause(.001)
+    for i in range(10):
+        for j in range(12):
+            print(i, j)
+            image.fill(i, j, (0, 1, 1))
+            plt.imshow(image.image)
+            plt.pause(.001)
+    plt.show()
+
+
 if __name__ == '__main__':
     # graph_basic_test()
     # test_grid_graph()
-    test_maze_generator()
+    # test_maze_generator()
+    test_matrix_image()
