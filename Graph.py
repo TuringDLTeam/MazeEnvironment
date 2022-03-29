42070566fa60aa5c0e4cef1ddc24fa02b33cc28d
Pooria Azami <pooriaazami@gmail.com>
2022-03-29 17:45:16 +0430

    edge operations added

--- Graph.py
+++ Graph.py
@@ -1,14 +1,21 @@
 class Graph:
     def __init__(self, nodes=0):
         self.__count_of_nodes = nodes
+        self.__edges = 0
         self.__edge_dict = {}
 
     def add_node(self, count=1):
         self.__count_of_nodes += count
+
+    def add_edge(self, head, tail):
+        self.__edge_dict[self.__edges] = (head, tail)
+
+    def remove_edge(self, edge):
+        del self.__edges[edge]
 
     @property
     def nodes(self):
         return self.__count_of_nodes
 
     def __str__(self):
-        return f'Graph(\n\t#Nodes: {self.__count_of_nodes},\t#Edges: {len(self.__edge_dict.keys())}\n)'
+        return f'Graph(\n\t#Nodes: {self.__count_of_nodes},\t#Edges: {self.__edges}\n)'
