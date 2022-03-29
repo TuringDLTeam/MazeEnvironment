d3794a739223cfd7a9c26e39ae28d176444fdb7b
Pooria Azami <pooriaazami@gmail.com>
2022-03-29 22:49:36 +0430

    monitor test added

--- Main.py
+++ Main.py
@@ -1,12 +1,11 @@
+import random
+
 from EdgeGraph import EdgeGraph
 from MatrixImage import MatrixImage
+from MazeMonitor import MazeMonitor
 from utils import initialize_grid_graph, generate_maze
 
 import matplotlib.pyplot as plt
-
-
-def main():
-    pass
 
 
 def graph_basic_test():
@@ -28,7 +27,8 @@ def test_grid_graph():
 
 
 def test_maze_generator():
-    print(generate_maze(10, 10))
+    maze, _ = generate_maze(10, 10)
+    print(maze)
 
 
 def test_matrix_image():
@@ -81,9 +81,35 @@ def test_matrix_image_tools():
     plt.show()
 
 
+def test_maze_monitor():
+    width = 20
+    height = 20
+    maze, paths = generate_maze(width, height)
+    # print(paths)
+    monitor = MazeMonitor(width, height, paths)
+    # print(maze)
+
+    fig = plt.gca()
+
+    x_axis = fig.axes.get_xaxis()
+    x_axis.set_visible(False)
+
+    y_axis = fig.axes.get_yaxis()
+    y_axis.set_visible(False)
+
+    plt.imshow(monitor.image)
+    plt.show()
+
+    monitor.move_agent((1, 1))
+    plt.imshow(monitor.image)
+    plt.show()
+
+
 if __name__ == '__main__':
+    random.seed(100)
     # graph_basic_test()
     # test_grid_graph()
     # test_maze_generator()
     # test_matrix_image()
-    test_matrix_image_tools()
+    # test_matrix_image_tools()
+    test_maze_monitor()
