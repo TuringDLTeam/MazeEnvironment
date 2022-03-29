8ac18aa0d27bf7b29a93ebdbfdaeefb7ac5b2e23
Pooria Azami <pooriaazami@gmail.com>
2022-03-29 18:21:48 +0430

    initialize_grid_graph test added

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-from EdgeGraph import Graph
+from EdgeGraph import EdgeGraph
+from utils import initialize_grid_graph
 
 
 def main():
@@ -6,12 +7,21 @@ def main():
 
 
 def graph_basic_test():
-    graph = Graph(10)
+    graph = EdgeGraph(10)
     print(graph)
 
     graph.add_node(2)
     print(graph)
 
 
+def test_grid_graph():
+    graph = initialize_grid_graph(4, 3)
+    print(graph)
+
+    for number, edge in graph.edges.items():
+        print(number, edge)
+
+
 if __name__ == '__main__':
-    graph_basic_test()
+    # graph_basic_test()
+    test_grid_graph()
